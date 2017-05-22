29b7fed35fd24debc805858c7faae208393f11d5
Janita <zhuchenjian@huizhi-tech.com>
2017-05-22 11:24:38 +0800

    aop-log

--- log-by-aop/src/main/java/com/janita/log/StepReadMe.java
+++ log-by-aop/src/main/java/com/janita/log/StepReadMe.java
@@ -3,7 +3,7 @@ package com.janita.log;
 /**
  * Created by Janita on 2017-05-22 10:25
  */
-public class Step {
+public class StepReadMe {
 
     /**
      * 1.<artifactId>spring-boot-starter-aop</artifactId>
---
+++ log-by-aop/src/test/java/com/janita/log/AopLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janita.log;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by Janita on 2017-05-22 11:22
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AopLogApplicationTests {
+}
