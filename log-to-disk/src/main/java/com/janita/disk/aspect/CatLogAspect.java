0aa0a93b287e38e903ea82cadb88f662fdf7ce06
janforp <janforp@163.com>
2017-10-25 15:46:58 +0800

    是打发

--- log-to-disk/src/main/java/com/janita/disk/aspect/CatLogAspect.java
+++ log-to-disk/src/main/java/com/janita/disk/aspect/CatLogAspect.java
@@ -1,6 +1,5 @@
 package com.janita.disk.aspect;
 
-import com.janita.disk.controller.CatController;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
